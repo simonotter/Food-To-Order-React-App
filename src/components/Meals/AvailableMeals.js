4d467835409a48cf7fb8bf9691ba77cb6e8c1bb0
Simon Otter <simon.otter@gmail.com>
2023-01-06 15:55:18 +0000

    Separate Meal items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,6 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
 import styles from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -28,11 +31,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };

4e30070469b3f9c19fa855fbba0ead8217b6a70a
Simon Otter <simon.otter@gmail.com>
2023-01-06 16:17:12 +0000

    Correct add same meal bug regardless of Added meal

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,11 @@
 import Input from '../../UI/Input';
 import styles from './MealItemForm.module.css';
 
-const MealItemForm = () => {
+const MealItemForm = props => {
   return (
     <form className={styles.form}>
       <Input label="Amount" input={{
-        id: 'amount',
+        id: 'amount_' + props.id,
         type: 'number',
         min: '1',
         max: '5',

3572a215df30d14204d4d752c1abcf2d3839726d
Simon Otter <simon.otter@gmail.com>
2023-01-06 16:10:54 +0000

    Add form to add a Meal to Cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import styles from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
   const price = `£${props.price.toFixed(2)}`;
@@ -11,7 +12,7 @@ const MealItem = (props) => {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-        
+        <MealItemForm />
       </div>
     </li>
   );
